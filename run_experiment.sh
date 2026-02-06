7db33d0a89eac020e8ed70cf19380fae824f7cb2
Jorge Sarrato <jorgesarrato@gmail.com>
2026-02-06 10:12:30 +0000

    sbatch file

---
+++ run_experiment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#############################
+#SBATCH -J FM
+#SBATCH -n 2
+#SBATCH -t 0-06:00:00
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH --gres gpu:1
+#SBATCH --mem 20G
+
+#SBATCH -D .
+#############################
+
+source mnist_env/bin/activate
+python -u src/run_experiment.py
